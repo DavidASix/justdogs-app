c781bb5f618ed4b3d6fc2843d47cab85b1861b0e
davidasix <davidandersonsix@gmail.com>
2023-10-06 17:40:13 -0400

    Renamed android package name in all applicable files

--- android/app/src/main/java/com/dave6/stroller/justdogs/MainActivity.java
+++ android/app/src/main/java/com/dave6/stroller/justdogs/MainActivity.java
@@ -1,4 +1,4 @@
-package com.justdogs;
+package com.dave6.stroller.justdogs;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/dave6/stroller/justdogs/MainApplication.java
+++ android/app/src/main/java/com/dave6/stroller/justdogs/MainApplication.java
@@ -1,4 +1,4 @@
-package com.justdogs;
+package com.dave6.stroller.justdogs;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
