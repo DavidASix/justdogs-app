6485fc1ab4acc8704e7445b5825cc698a4c53f83
davidasix <davidandersonsix@gmail.com>
2023-10-06 12:56:48 -0400

    Updating to modern React Native version

--- react-native.config.js
+++
@@ -1,9 +0,0 @@
-// react-native.config.js
-module.exports = {
-  dependency: {
-    platforms: {
-      android: null, // disable Android autolinking
-      ios: null //disable iOS autolinking
-    },
-  }
-};
