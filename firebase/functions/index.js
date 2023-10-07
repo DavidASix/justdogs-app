c8009add989471af5de5148d07a6b84a7df7e00d
davidasix <davidandersonsix@gmail.com>
2023-10-07 16:39:52 -0400

    Moved logic to get user ref to a new function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,29 +1,43 @@
 const {onRequest} = require('firebase-functions/v2/https');
-//const logger = require('firebase-functions/logger');
+
 const admin = require('firebase-admin');
+const firestore = admin.firestore();
 
 admin.initializeApp();
-const firestore = admin.firestore()
+
+
+getUserRef = (uid) => new Promise(async (resolve, reject) => {
+    // Returns a ref to the user document. Data obj can be accessed with .data();
+    try {
+      let user = null;
+      const usersCollection = firestore().collection('users');
+      const query = usersCollection.where('uid', '==', uid).limit(1);
+      const snapshot = await query.get();
+      if (!snapshot.empty) {
+        user = snapshot.docs[0];
+      } else {
+        throw new Error('User not found');
+      }
+      return resolve(user);
+    } catch (err) {
+      return reject(err);
+    }
+  });
+
 
 exports.updateUserEmail = onRequest(async (req, res) => {
   const {email, uid} = req.body;
   if (!email || !uid) {
-    return res.status(400).json({
-      success: false,
-      message: 'Missing email or UID in the request body',
-    });
+    return res.status(400).json({success: false, message: 'Missing request body',});
   }
   // Returns a ref to the user document. Data obj can be accessed with .data();
+  let user = null;
   try {
-    let user = null;
-    const usersCollection = firestore.collection('users');
-    const query = usersCollection.where('uid', '==', uid).limit(1);
-    const snapshot = await query.get();
-    if (!snapshot.empty) {
-      user = snapshot.docs[0];
-    } else {
-      res.status(404).json({success: false, message: 'User does not exist'});
-    }
+      user = await getUserRef(uid);
+  } catch (err) {
+      res.status(404).json({success: false, message: 'Could not find user'});
+  }
+  try {
     await user.ref.update({email});
     res.status(200).json({success: true, message: 'Email added'});
   } catch (err) {
