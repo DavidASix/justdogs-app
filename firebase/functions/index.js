8364d56989e50c58bda015397441b6456a9f0fd7
davidasix <davidandersonsix@gmail.com>
2023-10-07 17:19:22 -0400

    Added function to generate, store, and send out redemption code

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,16 +1,19 @@
 const {onRequest} = require('firebase-functions/v2/https');
-
+//const logger = require('firebase-functions/logger');
 const admin = require('firebase-admin');
-const firestore = admin.firestore();
+const axios = require('axios')
 
 admin.initializeApp();
+const firestore = admin.firestore();
 
+const p = require('./p.json')
+const restoreCodeEmail = require('./restoreCodeEmail')
 
 getUserRef = (uid) => new Promise(async (resolve, reject) => {
     // Returns a ref to the user document. Data obj can be accessed with .data();
     try {
       let user = null;
-      const usersCollection = firestore().collection('users');
+      const usersCollection = firestore.collection('users');
       const query = usersCollection.where('uid', '==', uid).limit(1);
       const snapshot = await query.get();
       if (!snapshot.empty) {
@@ -23,7 +26,6 @@ getUserRef = (uid) => new Promise(async (resolve, reject) => {
       return reject(err);
     }
   });
-
 
 exports.updateUserEmail = onRequest(async (req, res) => {
   const {email, uid} = req.body;
@@ -42,5 +44,54 @@ exports.updateUserEmail = onRequest(async (req, res) => {
     res.status(200).json({success: true, message: 'Email added'});
   } catch (err) {
     res.status(500).json({success: false, message: 'Server Error'});
+  }
+});
+
+exports.sendRestoreCode = onRequest(async (req, res) => {
+  const {uid} = req.body;
+  if (!uid) {
+      return res.status(400).json({success: false, message: 'Missing UID'});
+  }
+  // Create a random 5 character code
+  const code = [...Array(5)].map(() => Math.random().toString(36)[2]).join('').toUpperCase();
+  
+  // Get the user reference
+  let user = null;
+  try {
+    user = await getUserRef(uid);
+  } catch (err) {
+    return res.status(404).json({success: false, message: 'Could not find user'});
+  }
+  
+  // Save the new code to the user
+  try {
+    await user.ref.update({code, codeValid: true});
+  } catch (err) {
+    return res.status(500).json({success: false, message: 'Server Error'});
+  }
+  
+  // Create email paramters for MailGun
+  const credentials = btoa(`api:${p.keys.mg_api}`);
+  const mg_config = {
+          headers: {
+          'Authorization': `Basic ${credentials}`,
+          'Content-Type': 'multipart/form-data'
+      }
+  };
+  const content = {
+      from: `Just Dogs <JustDogs@${p.urls.mg_base}>`,
+      to: user.data().email,
+      subject: 'Just Dogs Recovery Code',
+      text: `Thank you for your support! Your Purchase Recovery Code is: ${code}`,
+      html: restoreCodeEmail(code)
+  }
+  
+  // Email the user the new code
+  try {
+    await axios.post(`${p.urls.mg}/messages`, content, mg_config);
+    return res.status(200).json({success: true, message: 'Email Sent'});
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({success: false, message: 'Error sending email'});
   }
 });
