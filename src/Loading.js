96582d4a3bfe1c593fe82d0e6db5917db8cc1208
davidasix <davidandersonsix@gmail.com>
2023-10-07 10:06:07 -0400

    Updated store email logic to use Firebase

--- src/Loading.js
+++ src/Loading.js
@@ -17,7 +17,6 @@ class Loading extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uid: false,
       loading: true,
       showAds: true,
       showCollectEmailModal: false,
@@ -60,14 +59,15 @@ class Loading extends React.Component {
     */
   }
 
-  getUserInformation = (uid) => new Promise(async (resolve, reject) => {
+  getUserRef = (uid) => new Promise(async (resolve, reject) => {
+    // Returns a ref to the user document. Data obj can be accessed with .data();
     try {
       let user = null;
       const usersCollection = firestore().collection('users');
       const query = usersCollection.where('uid', '==', uid).limit(1);
       const snapshot = await query.get();
       if (!snapshot.empty) {
-        user = snapshot.docs[0].data();
+        user = snapshot.docs[0];
       } else {
         throw new Error('User not found');
       }
@@ -84,7 +84,8 @@ class Loading extends React.Component {
       // check if stored UID is on server, if not send back a new one
       console.log('Try entered', uid)
       try {
-        const user = await this.getUserInformation(uid);
+        let user = await this.getUserRef(uid);
+        user = user.data();
         uid = user.uid;
       } catch (err) {
         uid = [...Array(32)].map(() => Math.random().toString(36)[2]).join('');
@@ -108,20 +109,9 @@ class Loading extends React.Component {
     // This is a callback called by the emailCollection modal when the email is submitted
       if (!email.match(c.regex.email)) reject({ title: 'Invalid Email', body: 'Please enter a valid email' });
       try {
-        // Get users UID from state, if that isn't available get it from storage then the server
-        let uid;
-        if (this.state.uid) {
-          uid = this.state.uid;
-        } else {
-          // TODO: Convert this to a firebase call, uncomment async store code
-          let storedUid = await AsyncStorage.getItem('@uid')
-          // check if stored UID is on server, if not send back a new one
-          //let { data } = await axios.post(`${c.urls.dave}checkUser`, { webkey: c.webkey, package: c.pn, uid: storedUid });
-          // Store the UID returned from the server
-          //await AsyncStorage.setItem('@uid', data.uid);
-          //uid = data.uid;
-        }
-        // await axios.post(`${c.urls.dave}storeEmail`, { webkey: c.webkey, uid, email })
+        let uid = await this.getUid();
+        const user = await this.getUserRef(uid);
+        user.ref.update({ email });
         resolve(true)
       } catch (err) {
         reject({ title: 'Server Error', body: 'Try again later' });
